32c4b81d633de03f29b09ebdd25240fa1cf49275
444Kosta444 <szilardbelan.777@gmail.com>
2025-08-09 10:02:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
-const CACHE = 'hadik-plus-sw-v1';
-const ASSETS = ['./', './index.html'];
+const CACHE = 'hadik-plus-sw-v4';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
-
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request).catch(() => caches.match('./index.html')))
   );
